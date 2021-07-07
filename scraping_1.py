ca01087e6efcc8dab8bd0b6e78d86d5ba97a3ad7
KamiKoze <44117546+KamiKoze@users.noreply.github.com>
2021-07-07 08:42:49 -0400

    started csv
    Added scraping of the title.
    Started to export results to a csv.

--- scraping_1.py
+++ scraping_1.py
@@ -1,22 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+
+with open('ria_scraping.csv', mode='w') as ria_scraping:
+    ria_scraping = csv.writer(ria_scraping, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    ria_scraping.writerow('URL' 'Title' 'Article')
 
 #URL = "https://ria.ru/"
 
 URL = "https://ria.ru/20210705/more-1740010917.html"
+print(URL)
 
-page= requests.get(URL)
-
-# =============================================================================
-# Output to Console
-# =============================================================================
-
-# print(page.text)
-
+page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find("div", itemprop="articleBody")
+title = soup.find("title")
+print(title)
 
-#job elements = results.find_all("div", class="schema_org")
+article = soup.find("div", itemprop="articleBody")
+print(article)
 
-print(results)
+ria_scraping.writerow([URL], [title], [article])
